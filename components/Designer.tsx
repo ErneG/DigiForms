afd4eba2e9be1b4912d919b77d895864b3de4441
GrumpySquiggle <46605904+SolidDeath@users.noreply.github.com>
2023-10-09 12:53:24 +0300

    Styled Designer sidebar, added Designer

--- components/Designer.tsx
+++ components/Designer.tsx
@@ -1,9 +1,21 @@
 'use client';
 
 import React from 'react';
+import DesignerSidebar from './DesignerSidebar';
 
 function Designer() {
-    return <div className="flex w-full h-full">Designer</div>;
+    return (
+        <div className="flex w-full h-full">
+            <div className="p-4 w-full">
+                <div className="bg-background max-w-[920ox] h-full m-auto flex flex-col flex-grow items-center justify-start flex-1 overflow-y-auto">
+                    <p className="text-3xl text-muted-foreground flex flex-grow items-center font-bold">
+                        Drop here
+                    </p>
+                </div>
+            </div>
+            <DesignerSidebar />
+        </div>
+    );
 }
 
 export default Designer;
