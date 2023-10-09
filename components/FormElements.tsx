10ae3a4070dc8d97e526b58691691daefcc259c4
GrumpySquiggle <46605904+SolidDeath@users.noreply.github.com>
2023-10-09 13:41:19 +0300

    Fixed a type

--- components/FormElements.tsx
+++ components/FormElements.tsx
@@ -8,7 +8,7 @@ export type FormElement = {
     construct: (id: string) => FormElementInstance;
 
     designerBtnElement: {
-        icon: React.ReactElement;
+        icon: React.ElementType;
         label: string;
     };
 
