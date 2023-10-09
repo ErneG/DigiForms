9cf675ea76ed48a83937f5a4e212afe50967e626
GrumpySquiggle <46605904+SolidDeath@users.noreply.github.com>
2023-10-09 12:57:52 +0300

    Wrapped form with DndContext

--- components/FormBuilder.tsx
+++ components/FormBuilder.tsx
@@ -10,31 +10,34 @@ import PreviewDialogButton from './PreviewDialogButton';
 import PublishFormButton from './PublishFormButton';
 import SaveFormButton from './SaveFormButton';
 import Designer from './Designer';
+import { DndContext } from '@dnd-kit/core';
 // COMPONENTS
 
 function FormBuilder({ form }: { form: Form }) {
     return (
-        <main className="flex flex-col w-full">
-            <nav className="flex justify-between border-b-2 p-4 gap-3 items-center">
-                <h2 className="trucate font-medium">
-                    <span className="text-muted-foreground mr-2">
-                        {form.name}
-                    </span>
-                </h2>
-                <div className="flex items-center gap-2">
-                    <PreviewDialogButton />
-                    {!form.published && (
-                        <>
-                            <SaveFormButton />
-                            <PublishFormButton />
-                        </>
-                    )}
+        <DndContext>
+            <main className="flex flex-col w-full">
+                <nav className="flex justify-between border-b-2 p-4 gap-3 items-center">
+                    <h2 className="trucate font-medium">
+                        <span className="text-muted-foreground mr-2">
+                            {form.name}
+                        </span>
+                    </h2>
+                    <div className="flex items-center gap-2">
+                        <PreviewDialogButton />
+                        {!form.published && (
+                            <>
+                                <SaveFormButton />
+                                <PublishFormButton />
+                            </>
+                        )}
+                    </div>
+                </nav>
+                <div className="flex w-full flex-grow items-center justify-center relative overflow-y-auto h-[200px]  bg-accent bg-[url(/paper.svg)] dark:bg-[url(/paper-dark.svg)]">
+                    <Designer />
                 </div>
-            </nav>
-            <div className="flex w-full flex-grow items-center justify-center relative overflow-y-auto h-[200px]  bg-accent bg-[url(/paper.svg)] dark:bg-[url(/paper-dark.svg)]">
-                <Designer />
-            </div>
-        </main>
+            </main>
+        </DndContext>
     );
 }
 
