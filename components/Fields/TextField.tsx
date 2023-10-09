edcccc9fa1b6028d17e22553885a2bdae5e28f20
GrumpySquiggle <46605904+SolidDeath@users.noreply.github.com>
2023-10-09 13:39:10 +0300

    Added form component type files

---
+++ components/Fields/TextField.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { MdTextFields } from 'react-icons/md';
+import { ElementsType, FormElement } from '../FormElements';
+
+const type: ElementsType = 'TextField';
+
+export const TextFieldFormElement: FormElement = {
+    type,
+    construct: (id: string) => ({
+        id,
+        type,
+        extraAttributes: {
+            label: 'Text field',
+            helperText: 'Helper text',
+            required: false,
+            placeholder: 'Value here...'
+        }
+    }),
+    designerBtnElement: {
+        icon: MdTextFields,
+        label: 'Text Field'
+    },
+    designerComponent: () => <div>Designer component</div>,
+    formComponent: () => <div>Form component</div>,
+    propertiesComponent: () => <div>Properties component</div>
+};
