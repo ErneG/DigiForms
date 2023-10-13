4f4e8f39e1f949955dc2d12b4c67d052cb54dfd8
GrumpySquiggle <46605904+SolidDeath@users.noreply.github.com>
2023-10-13 17:21:35 +0300

    Added field displaying

--- components/Designer.tsx
+++ components/Designer.tsx
@@ -5,7 +5,11 @@ import DesignerSidebar from './DesignerSidebar';
 import { DragEndEvent, useDndMonitor, useDroppable } from '@dnd-kit/core';
 import { cn } from '@/lib/utils';
 import useDesigner from './hooks/useDesigner';
-import { ElementsType, FormElements } from './FormElements';
+import {
+    ElementsType,
+    FormElementInstance,
+    FormElements
+} from './FormElements';
 import { idGenerator } from '@/lib/idGenerator';
 
 function Designer() {
@@ -17,6 +21,7 @@ function Designer() {
         }
     });
 
+    console.log('Elements', elements);
     useDndMonitor({
         //can be used because the whole FormBuilder is wrapped in a DndContext
         onDragEnd: (event: DragEndEvent) => {
@@ -24,12 +29,13 @@ function Designer() {
             if (!active || !over) return;
 
             const isDesignerBtnELement =
-                active?.data?.current?.isDesignerBtnElement;
+                active?.data?.current?.isDesignerBtnELement;
             if (isDesignerBtnELement) {
                 const type = active.data?.current?.type;
                 const newElement = FormElements[type as ElementsType].construct(
                     idGenerator()
                 );
+                addElement(0, newElement);
                 console.log('NEW ELEMENT', newElement);
             }
             console.log('drag event', event);
@@ -45,7 +51,7 @@ function Designer() {
                         droppable.isOver && 'ring-2 ring-primary/20'
                     )}
                 >
-                    {!droppable.isOver && (
+                    {!droppable.isOver && elements.length === 0 && (
                         <p className="text-3xl text-muted-foreground flex flex-grow items-center font-bold">
                             Drop here
                         </p>
@@ -55,11 +61,25 @@ function Designer() {
                             <div className="h-[120px] rounded-md bg-primary/20"></div>
                         </div>
                     )}
+                    {elements.length > 0 && (
+                        <div className="flex flex-col w-full gap-2 p-4">
+                            {elements.map((element) => (
+                                <DesignerElementWrapper
+                                    key={element.id}
+                                    element={element}
+                                />
+                            ))}
+                        </div>
+                    )}
                 </div>
             </div>
             <DesignerSidebar />
         </div>
     );
 }
+function DesignerElementWrapper({ element }: { element: FormElementInstance }) {
+    const DesignerElement = FormElements[element.type].designerComponent;
 
+    return <DesignerElement elementInstance={element} />;
+}
 export default Designer;
--- components/Fields/TextField.tsx
+++ components/Fields/TextField.tsx
@@ -1,27 +1,62 @@
 'use client';
 
 import { MdTextFields } from 'react-icons/md';
-import { ElementsType, FormElement } from '../FormElements';
+import {
+    ElementsType,
+    FormElement,
+    FormElementInstance
+} from '../FormElements';
+import { Label } from '../ui/label';
+import { Input } from '../ui/input';
 
 const type: ElementsType = 'TextField';
+
+const extraAttributes = {
+    label: 'Text field',
+    helperText: 'Helper text',
+    required: false,
+    placeHolder: 'Value here...'
+};
 
 export const TextFieldFormElement: FormElement = {
     type,
     construct: (id: string) => ({
         id,
         type,
-        extraAttributes: {
-            label: 'Text field',
-            helperText: 'Helper text',
-            required: false,
-            placeholder: 'Value here...'
-        }
+        extraAttributes
     }),
     designerBtnElement: {
         icon: MdTextFields,
         label: 'Text Field'
     },
-    designerComponent: () => <div>Designer component</div>,
+    designerComponent: DesignerComponent,
     formComponent: () => <div>Form component</div>,
     propertiesComponent: () => <div>Properties component</div>
 };
+
+type CustomInstance = FormElementInstance & {
+    extraAttributes: typeof extraAttributes;
+};
+
+function DesignerComponent({
+    elementInstance
+}: {
+    elementInstance: FormElementInstance;
+}) {
+    const element = elementInstance as CustomInstance;
+    const { label, require, placeHolder, helperText } = element.extraAttributes;
+    return (
+        <div className="flex flex-col gap-2 w-full">
+            <Label>
+                {element.extraAttributes.label}
+                {element.extraAttributes.required && '*'}
+            </Label>
+            <Input readOnly disabled placeholder={placeHolder} />
+            {helperText && (
+                <p className="text-muted-foreground text-[0.8rem]">
+                    {helperText}
+                </p>
+            )}
+        </div>
+    );
+}
