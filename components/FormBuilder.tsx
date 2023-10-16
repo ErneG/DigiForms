9aa1ad83fa9bb4729cdd28957c5482ea06eb76ac
GrumpySquiggle <46605904+SolidDeath@users.noreply.github.com>
2023-10-16 19:16:12 +0300

    Added dnd sensors to distinguish between a click and drag motions

--- components/FormBuilder.tsx
+++ components/FormBuilder.tsx
@@ -10,13 +10,20 @@ import PreviewDialogButton from './PreviewDialogButton';
 import PublishFormButton from './PublishFormButton';
 import SaveFormButton from './SaveFormButton';
 import Designer from './Designer';
-import { DndContext } from '@dnd-kit/core';
+import { DndContext, MouseSensor, useSensor, useSensors } from '@dnd-kit/core';
 import DragOverlayWrapper from './DragOverlayWrapper';
 // COMPONENTS
 
 function FormBuilder({ form }: { form: Form }) {
+    const mouseSensor = useSensor(MouseSensor, {
+        activationConstraint: {
+            distance: 10 //activates the dragging motion only if moved by more than 10 px
+        }
+    });
+
+    const sensors = useSensors(mouseSensor);
     return (
-        <DndContext>
+        <DndContext sensors={sensors}>
             <main className="flex flex-col w-full">
                 <nav className="flex justify-between border-b-2 p-4 gap-3 items-center">
                     <h2 className="trucate font-medium">
